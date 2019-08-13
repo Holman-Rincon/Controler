35f133333dd6a939aa0412ee2e55a0a2c3962064
Holman Rincón <holmanrincon7@gmail.com>
2019-08-13 15:46:59 -0500

    commit 3

--- sisVentas/resources/views/layouts/admin.blade.php
+++ sisVentas/resources/views/layouts/admin.blade.php
@@ -173,12 +173,17 @@
               </ul>
             </li>
 
-
             <li class="treeview">
               <a href="#">
                 <i class="fa fa-bar-chart"></i>
                 <span>Reportes</span>
+                 <i class="fa fa-angle-left pull-right"></i>
               </a>
+              <ul class="treeview-menu">
+                <li><a href="compras/ingreso"><i class="fa fa-circle-o"></i> Inventario</a></li>
+                <li><a href="compras/ingreso"><i class="fa fa-circle-o"></i> Ventas</a></li>
+                <li><a href="compras/proveedor"><i class="fa fa-circle-o"></i> Pedidos</a></li>
+              </ul>
             </li>
 
             <li class="treeview">
